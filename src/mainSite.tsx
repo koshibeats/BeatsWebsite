c3c3c0b9e99f8647db0488fd13f12f8b7015feb1
koshibeats <98182961+koshibeats@users.noreply.github.com>
2022-01-28 14:07:12 +0100

    added mainsite

---
+++ src/mainSite.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import App from './App';
+
+function mainSite() {
+	return (
+		<div className="App">
+			<p> HALLOOSKODKSODKOSKDKOKSOkok</p>
+		</div>
+	);
+}
+
+export default mainSite;
